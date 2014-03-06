0622bbce02d8f2bc59d32b2ac768a7ea57ce474b
Evgeny Poberezkin <evgeny.poberezkin@mailonline.co.uk>
2014-03-06 11:13:22 +0000

    evgeny, tom: fixed CCImagePreviewList/imageList

--- Form.js
+++ Form.js
@@ -370,7 +370,7 @@ var itemsClasses = {
     previewimage: 'CCPreviewImage',
     previewcropall: 'CCPreviewCropAll',
     droptarget: 'MLDropTarget',
-    imagelist: 'CCModuleImagePreview'
+    imagelist: 'CCImagePreviewList'
 };
 
 /**
