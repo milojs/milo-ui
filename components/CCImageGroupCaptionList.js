c71e982e4a4ab8053e6c66fedc6588907143633f
Luis Silva <luis.dasilva@mailonline.co.uk>
2014-06-09 17:28:27 +0100

    [Luis] fix imageGroup captions, add new design

--- components/CCImageGroupCaptionList.js
+++ components/CCImageGroupCaptionList.js
@@ -4,83 +4,16 @@ var componentsRegistry = milo.registry.components
     , Component = componentsRegistry.get('Component')
     , logger = milo.util.logger;
 
-var IMAGEGROUPCAPTION_CHANGE_MESSAGE = 'ccimagegroupcaptionchange';
-
 var CCImageGroupCaptionList = Component.createComponentClass('CCImageGroupCaptionList', {
     container: undefined,
     events: undefined,
+    list: undefined,
     dom: {
-        cls: 'cc-imagegroup-group'
+        cls: 'cc-imagegroup-group',
+        tagName: 'ul'
     },
-    data: {
-        get: CCImageGroupCaptionList_get,
-        set: CCImageGroupCaptionList_set,
-        del: CCImageGroupCaptionList_del,
-        splice: CCImageGroupCaptionList_splice,
-        event: IMAGEGROUPCAPTION_CHANGE_MESSAGE
-    },
-    model: undefined
-    //model: {
-    //    messages: {
-    //        '*': {
-    //            subscriber: _.throttle(onListUpdated, 100),
-    //            context: 'owner'
-    //        },
-    //        '**': {
-    //            subscriber: _.throttle(onInnerChange, 50),
-    //            context: 'owner'
-    //        }
-    //    }
-    //}
+    data: undefined
 });
 
 componentsRegistry.add(CCImageGroupCaptionList);
 module.exports = CCImageGroupCaptionList;
-
-_.extendProto(CCImageGroupCaptionList, {
-    init: CCImageGroupCaptionList$init
-});
-
-
-function CCImageGroupCaptionList$init() {
-    Component.prototype.init.apply(this, arguments);
-    this.once('childrenbound', onChildrenBound);
-}
-
-
-function onChildrenBound() {
-    //milo.minder(this.container.scope.imagesList.data, '<<<->>>', this.model);
-}
-
-
-function CCImageGroupCaptionList_get() {
-    var model = this.model.get();
-    return model ? _.deepClone(model) : [];
-}
-
-
-function CCImageGroupCaptionList_set(value) {
-    console.log('CCImageGroupCaptionList_set', value);
-    this.model.set(value || []);
-    sendChangeMessage.call(this);
-}
-
-
-function CCImageGroupCaptionList_del() {
-    var res = this.model.set([]);
-    sendChangeMessage.call(this);
-    return res;
-}
-
-
-function CCImageGroupCaptionList_splice(index, howmany) { // ... arguments
-    console.log('CCImageGroupCaptionList_splice', index, howmany);
-    var args = [index, howmany].concat(Array.prototype.slice.call(arguments, 2));
-
-    this.model.splice.apply(this.model, args);
-    sendChangeMessage.call(this);
-}
-
-function sendChangeMessage() {
-    this.data.getMessageSource().dispatchMessage(IMAGEGROUPCAPTION_CHANGE_MESSAGE);
-}
