a72f478b016840511c0fc43bbeb48ac038b19c91
Szilveszter Szilagyi <szilveszter.szilagyi@mailonline.co.uk>
2014-07-29 13:45:15 +0100

    rebase css

--- components/CCArticlePreviewList.js
+++ components/CCArticlePreviewList.js
@@ -29,23 +29,23 @@ var CCArticlePreviewList = Component.createComponentClass('CCArticlePreviewList'
                 <span class="bocc channel-bar"></span>\
                 <div>\
                     <div>\
-                        <div class="cc-article-top-bar cc-table cc-table-bs3 cc-width-100">\
-                            <div class="cc-text-light cc-cell cc-bg-lightgrey cc-align-left">ID: <span class="cc-dark cc-text cc-crs-text" ml-bind="[data]:id"></span></div>\
-                            <div class="cc-text-light cc-cell cc-bg-lightgrey cc-align-center"> <span ml-bind="[data]:createdDate"></span></div>\
-                            <div class="cc-text-light cc-cell cc-bg-lightgrey cc-align-center">Modified: <span class="cc-dark" ml-bind="[data]:modifiedDate"></span></span></div>\
+                        <div class="cc-article-top-bar">\
+                            <div class="cc-id">ID: <span ml-bind="[data]:id"></span></div>\
+                            <div class="cc-created"> <span ml-bind="[data]:createdDate"></span></div>\
+                            <div class="cc-modified">Modified: <span ml-bind="[data]:modifiedDate"></span></span></div>\
                         </div>\
-                        <div class="cc-article-title-bar cc-clear cc-width-100 cc-pt5">\
-                            <div class="cc-dark-bg cc-text cc-fl cc-light cc-plr5 cc-mr5" ml-bind="[data]:status"></div>\
-                            <div class="cc-article-title cc-dark cc-bold" ml-bind="[data]:title"></div>\
+                        <div class="cc-article-title-bar">\
+                            <div class="cc-status" ml-bind="[data]:status"></div>\
+                            <div class="cc-article-title" ml-bind="[data]:title"></div>\
                         </div>\
-                        <div class="cc-article-short-body cc-clear cc-fl cc-width-100 cc-pt5">\
-                            <div class="cc-fl cc-mr5 cc-preview-image" ml-bind="[data container]:thumb">\
+                        <div class="cc-article-short-body">\
+                            <div class="cc-preview-image" ml-bind="[data container]:thumb">\
                                 <img ml-bind="[data]:hostUrl" class="media-object">\
                             </div>\
                             <div class="" ml-bind="[data]:previewText"></div>\
                         </div>\
-                        <div class="cc-article-bottom-bar cc-clear cc-pt5">\
-                            <div class="cc-text-light cc-fl">Created by: <span ml-bind="[data]:createdBy"></span></div>\
+                        <div class="cc-article-bottom-bar">\
+                            <div class="cc-created-by">Created by: <span ml-bind="[data]:createdBy"></span></div>\
                             <span class="cc-article-actions">\
                                 <span class="fa fa-search cc-debug" ml-bind="[events]:previewBtn" title="read-only article"></span>\
                                 <span class="cc-icon cc-clone-article-small-icon" ml-bind="[events]:cloneBtn" title="clone article"></span>\
