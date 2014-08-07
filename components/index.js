439a8ef1d94fa6a2a7301335e6347daf4ff37db3
Evgeny Poberezkin <evgeny.poberezkin@mailonline.co.uk>
2014-08-07 14:29:29 +0100

    [evgeny, luis] moved Toolbar and util to cc-services

---
+++ components/index.js
@@ -0,0 +1,16 @@
+'use strict';
+
+require('./CCPreviewImage');
+require('./CCPreviewCropAll');
+require('./CCArticleHistory');
+
+require('./CCModuleImagePreview');
+require('./CCModuleArticlePreview');
+require('./CCArticlePreviewList');
+require('./CCModuleVideoPreview');
+require('./CCPreviewList');
+require('./CCModuleArticleModulePreview');
+require('./CCArticleModulePreviewList');
+require('./CCLinkList');
+require('./CCRelatedList');
+require('./CCImageGroupCaptionList');
