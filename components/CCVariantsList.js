5654e0bd0229056dc836447a65feab71315854b9
Evgeny Poberezkin <evgeny.poberezkin@mailonline.co.uk>
2014-09-26 17:30:06 +0100

    new variants panel progress

--- components/CCVariantsList.js
+++ components/CCVariantsList.js
@@ -8,15 +8,16 @@ var componentsRegistry = milo.registry.components
     , countries = require('../../countries').toArray();
 
 
-var variantsTemplate = '<ul ml-bind="MLList:list"> \
-                            <li> \
-                                <span>All regions</span> \
-                                <span ml-bind=":defaultVisible">Visible</span> \
-                                <span ml-bind=":defaultExcludedWrapper"> \
-                                    <input ml-bind="MLInput:defaultExcluded" type="checkbox"> \
-                                    Excluded \
-                                </span> \
-                            <li ml-bind="MLListItem:item"> \
+var variantsTemplate = '<div> \
+                            <span>All regions</span> \
+                            <span ml-bind=":defaultVisible">Visible</span> \
+                            <span ml-bind=":defaultExcludedWrapper"> \
+                                <input ml-bind="MLInput:defaultExcluded" type="checkbox"> \
+                                Excluded \
+                            </span> \
+                        </div> \
+                        <ul ml-bind="MLList:variants"> \
+                            <li ml-bind="MLListItem:variant"> \
                                 <span class="cc-icon cc-delete-icon" ml-bind="[events]:deleteBtn"></span> \
                                 <span ml-bind="[data]:label"></span> \
                                 <input ml-bind="MLInput:excluded" type="checkbox"> \
@@ -27,10 +28,11 @@ var variantsTemplate = '<ul ml-bind="MLList:list"> \
 
 
 var CCVariantsList = Component.createComponentClass('CCVariantsList', {
+    container: undefined,
+    data: undefined,
     dom: {
         cls: ['cc-variants-list']
     },
-    container: undefined,
     events: undefined,
     template: {
         template: variantsTemplate,
@@ -43,7 +45,7 @@ module.exports = CCVariantsList;
 componentsRegistry.add(CCVariantsList);
 formRegistry.add('variantslist', {
     compClass: 'CCVariantsList',
-    modelPathRule: 'optional',
+    modelPathRule: 'required',
     itemFunction: processVariantsListSchema
 });
 
@@ -60,7 +62,6 @@ function CCVariantsList$init() {
 
 
 function onChildrenBound(msg, data) {
-    console.log('onChildrenBound');
     this.template.render().binder();
     var scope = this.container.scope
 
@@ -68,7 +69,7 @@ function onChildrenBound(msg, data) {
     addCountry.setOptions(countries);
     addCountry.data.on('', { subscriber: onAddCountry, context: this });
 
-    this._list = scope.list;
+    this._list = scope.variants;
     this._list.model.onMessages({
         '': { subscriber: onListChange, context: this }, // splice events
         '[*].excluded': {subscriber: onVariantExcluded, context: this }
@@ -83,33 +84,33 @@ function onAddCountry(msg, data) {
 
 
 function onListChange(msg, data) {
-    console.log('onListChange', msg, data);
+    // console.log('onListChange', msg, data);
 }
 
 
 function onVariantExcluded(msg, data) {
-    console.log('onVariantExcluded', msg, data);
+    // console.log('onVariantExcluded', msg, data);
 }
 
 
 function processVariantsListSchema(comp, schema) {
-    if (this.hasVariants()) {
-        var variants = comp._variants = this.getSecondaryVariants();
-        if (variants && variants.length) {
-            var listData = [];
-            variants.forEach(function(variant) {
-                var criteria = variant.getCriteria()
-                    , country = _.find(countries, function(c) {
-                        return criteria.geo == c.value;
-                    });
-                if (country) {
-                    var country = _.clone(country);
-                    country.excluded = variant.isExcluded();
-                    listData.push(country);
-                }
-            });
+    // if (this.hasVariants()) {
+    //     var variants = comp._variants = this.getSecondaryVariants();
+    //     if (variants && variants.length) {
+    //         var listData = [];
+    //         variants.forEach(function(variant) {
+    //             var criteria = variant.getCriteria()
+    //                 , country = _.find(countries, function(c) {
+    //                     return criteria.geo == c.value;
+    //                 });
+    //             if (country) {
+    //                 var country = _.clone(country);
+    //                 country.excluded = variant.isExcluded();
+    //                 listData.push(country);
+    //             }
+    //         });
 
-            _.deferMethod(comp._list.model, 'set', listData);
-        }
-    }
+    //         _.deferMethod(comp._list.model, 'set', listData);
+    //     }
+    // }
 }
