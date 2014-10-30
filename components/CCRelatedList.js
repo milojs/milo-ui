1f00b4aaaec24c95100900e9b5d98cbba0e206ba
Evgeny Poberezkin <evgeny.poberezkin@mailonline.co.uk>
2014-10-30 14:35:53 +0000

    Enter key submits value in related links and in embed, closes #1015

--- components/CCRelatedList.js
+++ components/CCRelatedList.js
@@ -52,7 +52,10 @@ function CCRelatedList$setLinkDefaults(defaultLink) {
 
 function onChildrenBound() {
     var saveBtn = this.container.scope.saveBtn;
-    saveBtn.events.on('click', { subscriber: onSaveButtonSubscriber, context: this });
+    saveBtn.events.on('click', { subscriber: onSaveButtonClick, context: this });
+
+    var input = this.container.scope.input;
+    input.events.on('keypress', { subscriber: onInputKey, context: this })
 
     this.events.on('click', { subscriber: onListClickSubscriber, context: this });
     this.events.on('change', { subscriber: onListChangeSubscriber, context: this });
@@ -107,7 +110,12 @@ function onListChangeSubscriber(type, event) {
 }
 
 
-function onSaveButtonSubscriber() {
+function onInputKey(msg, event) {
+    if (event.keyCode == 13) onSaveButtonClick.call(this);
+}
+
+
+function onSaveButtonClick() {
     var newRelated = this.container.scope.input.data.get()
         .split(' ').filter(function(value) {
             return value != '';
