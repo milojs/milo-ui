64cf1ae89e9c60a137555ac9edc81b6c293adf38
Szilveszter Szilagyi <szilveszter.szilagyi@mailonline.co.uk>
2015-05-18 10:52:11 +0100

    elementlock options

--- ElementLock.js
+++ ElementLock.js
@@ -6,24 +6,26 @@ _.extendProto(ElementLock, {
     unlock: ElementLock$unlock
 });
 
-function ElementLock(element, timeout) {
-    var timeout = timeout || 20000;
+function ElementLock(element, options) {
+    var timeout = Number.isInteger(options) ? options : options.timeout || 20000;
+    var position = options.position || 'absolute';
+    var backgroundColor = options.backgroundColor || 'rgba(255, 255, 255, 0.85)';
 
     var blankDiv = document.createElement('div');
     this.blankDiv = blankDiv;
     element.appendChild(blankDiv);
     var style = blankDiv.style;
-    style.position = 'absolute';
+    style.position = position;
     style.zIndex = 20;
     style.top = style.left = 0;
     style.width = style.height = '100%';
-    blankDiv.style.backgroundColor = 'rgba(255, 255, 255, 0.85)';
+    blankDiv.style.backgroundColor = backgroundColor;
 
     var spinner = document.createElement('div');
     blankDiv.appendChild(spinner);
     var spinnerStyle = spinner.style;
     spinner.className = 'cc-loading-spinner';
-    spinnerStyle.position = 'absolute';
+    spinnerStyle.position = position;
     spinnerStyle.top = spinnerStyle.left = 'calc(50% - 10px)';
     spinnerStyle.width = spinnerStyle.height = '20px';
 
