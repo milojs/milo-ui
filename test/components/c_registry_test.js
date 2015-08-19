3234e8caba6f87948a93a14ee714a62720bc9852
Evgeny Poberezkin <e.poberezkin@me.com>
2015-08-19 18:00:33 +0100

    remove files that are in milo

--- test/components/c_registry_test.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var componentsRegistry = require('../../lib/components/c_registry')
-    , Component = require('../../lib/components/c_class')
-    , testRegistry = require('../utils/test_registry');;
-
-describe('registry of components', function() {
-    testRegistry(componentsRegistry, Component);
-});
